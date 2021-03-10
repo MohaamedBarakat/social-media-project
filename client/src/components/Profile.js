4a3a3fae5cff43993544a8545a3b5bbb1efa617c
MohaamedBarakat <Mohammad.barakat70@gmail.com>
2021-03-10 19:28:11 +0200

    add delete feature to every post

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import photo from '../images/138675207_3988428944509572_4336521994899151105_o.jpg'
 const Profile = () => {
     const [isEdit,setIsEdit] =useState(false);
@@ -7,20 +7,46 @@ const Profile = () => {
     const [password,setPassword] = useState('');
     const [newPost,setNewPost] = useState('');
     const [postImage , setPostImage] = useState('');
-    const handleNewPost = () => {
-        fetch('url',{
+    const [posts,setPosts] = useState([{}]);
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/posts',{
+            method:'GET',
+            headers:{
+                Authorization:`Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res =>{
+            return res.json();
+        })
+        .then(recivedPosts =>{
+            setPosts(recivedPosts.posts.reverse());
+            console.log(posts);
+        }).catch(err => {
+           
+        })
+    },[]);
+    const handleNewPost = (e) => {
+        e.preventDefault();
+        console.log(newPost);
+        fetch('http://localhost:4000/new-post',{
             method:'PUT',
             headers:{
                 'Content-Type':'application/json',
                 Authorization:`Bearer ${localStorage.getItem('token')}`
             },
-            body:JSON.stringify()
+            body:JSON.stringify({
+                content:newPost
+            })
         })
         .then(res => {
-
+            return res.json();
+        })
+        .then(res =>{
+            setPosts(res.post.reverse());
         })
         .catch(err => {
-
+            console.log(err);
         })
     }
     const handleIsEditProfile = () => {
@@ -32,6 +58,22 @@ const Profile = () => {
     const handleEditProfile = (e) => {
         e.preventDefault();
         
+    }
+    const handleDeletePost = (id) => {
+        fetch('http://localhost:4000/'+id,{
+            method:'DELETE',
+            headers:{
+                Authorization:`Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res =>{
+            return res.json();
+        })
+        .then(data =>{
+            setPosts(data.post.reverse())
+        })
+        console.log(id);
+
     }
     return ( 
         <div className="profile">
@@ -55,15 +97,14 @@ const Profile = () => {
                                 onChange={(e)=>setPassword(e.target.value)}/>
                         <button className="update-profile">Update Profile</button>
                     </form>
-                 </div>
-                
+                 </div>  
             </div>}
 
             <div>
             <button className="edit-profile" onClick={handleIsEditProfile}>Edit profile</button>
             </div>
             <div className="new-post">
-                
+
                 <form onSubmit = {handleNewPost}>
 
                 <textarea className='status' type="text"
@@ -72,17 +113,25 @@ const Profile = () => {
                         onChange={(e) => setNewPost(e.target.value)}
                 
                 />
-
                 <input className='file' type="file" onChange={(e) => setPostImage(e.target.files[0])} />
 
-                <button>Post!</button>
+                <button>Post !</button>
 
                 </form>
+
+
+                {!posts && <p>No posts</p>}
+
+                {posts && posts.map( (post) =>
+                <div className="post-content" key={post._id}>
+                        <button className="btn-edit-post">...</button>
+                        <button className="btn-delete-post" onClick={()=>{handleDeletePost(post._id)}}>X</button>
+                        <p>{post.createdAt}</p>
+                        <li>{post.content}</li>
+                </div>
+                )}
             </div>
 
-            <div className="post">
-                No posts  
-            </div>
         </div>
      );
 }
